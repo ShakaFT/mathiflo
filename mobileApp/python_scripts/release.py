c4f5b38256de94d4e2e8cd39d60d4a82c59448f6
Florent TORNICELLI <florent@geode.com>
2023-01-28 20:07:10 +0100

    fix: fix loading pubspec.yaml file

--- mobileApp/python_scripts/release.py
+++ mobileApp/python_scripts/release.py
@@ -1,10 +1,11 @@
 """
 This script is used to locally deploy app.
 """
-import json
 import subprocess
+import sys
 
 from rich import print as shell_print
+import yaml
 
 import utils
 
@@ -25,13 +26,13 @@ def upgrade_version():
     This function increments code version.
     """
     with open("pubspec.yaml", "r", encoding="UTF-8") as file:
-        pubspec = json.load(file)
+        pubspec = yaml.load(file, Loader=yaml.FullLoader)
 
     splited_version = pubspec["version"].split("+")
-    pubspec["version"] = f"{splited_version[0]}+{splited_version[1] + 1}"
+    pubspec["version"] = f"{splited_version[0]}+{int(splited_version[1]) + 1}"
 
     with open("pubspec.yaml", "w", encoding="UTF-8") as file:
-        json.dump(pubspec, file)
+        yaml.dump(pubspec, file)
 
     utils.push("chore: new release --> upgrade code version")
 
@@ -46,6 +47,7 @@ def main():
         input("Press Enter to continue...")
     else:
         shell_print("[red]Exit ! You can send release only on master...")
+        sys.exit()
 
     upgrade_version()
     utils.set_config(ENVIRONNEMENT)
@@ -54,10 +56,11 @@ def main():
 
 
 if __name__ == "__main__":
-    utils.verify_environment()
-    try:
-        main()
-    except Exception as e:  # pylint: disable=broad-except
-        shell_print(f"[bold red]Exit with error : {str(e)}")
-    finally:
-        utils.reset()
+    main()
+    # utils.verify_environment()
+    # try:
+    #     main()
+    # except Exception as e:  # pylint: disable=broad-except
+    #     shell_print(f"[bold red]Exit with error : {str(e)}")
+    # finally:
+    #     utils.reset()
