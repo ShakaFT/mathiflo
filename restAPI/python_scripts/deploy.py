ef6fa22269ca991ae5eba73039c7a63f77efcb81
Florent TORNICELLI <florent@geode.com>
2023-03-08 00:32:02 +0100

    refactor: refactor python scripts

--- restAPI/python_scripts/deploy.py
+++ restAPI/python_scripts/deploy.py
@@ -10,30 +10,34 @@ from rich import print as shell_print
 import utils
 
 PROJECT_ID = "mathiflo" if utils.is_prod() else "mathiflo-dev"
-SERVICES: list[str] = utils.get_services()
 
 
-def deploy_services(services_to_deploy:list[str]):
+def deploy_services(services_to_deploy: list[str]):
     """
     This function deploys services.
     """
     for service in services_to_deploy:
         shell_print(f"[bold][magenta]\nI will deploy service : [green]{service}\n")
-        subprocess.call(f"gcloud app deploy --quiet --project {PROJECT_ID} services/{service}/app.yaml", shell=True)
+        subprocess.call(
+            f"gcloud app deploy --quiet --project {PROJECT_ID} services/{service}/app.yaml",
+            shell=True,
+        )
 
 
 def select_menu() -> list:
     """
     This function shows select menus and returns the services to deploy.
     """
+    services = utils.get_services()
+
     title = "Choose the service you want to deploy: "
     options = ["None", "All"]
-    options.extend(SERVICES)
+    options.extend(services)
     service_to_deploy, _ = pick(options, title)
 
     match service_to_deploy:
         case "All":
-            services_to_deploy = SERVICES
+            services_to_deploy = services
         case "None":
             services_to_deploy = []
         case _:
@@ -49,7 +53,9 @@ def main():
     main function
     """
     if utils.is_prod():
-        shell_print("[bold italic yellow on red blink]You really want to deploy in production ?")
+        shell_print(
+            "[bold italic yellow on red blink]You really want to deploy in production ?"
+        )
         input("Press Enter to continue...")
 
     services_to_deploy = select_menu()
