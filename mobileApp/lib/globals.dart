8dd1404cd63c690fc9196d9f02d89a65a95697a6
Florent TORNICELLI <florent@geode.com>
2023-03-22 18:59:02 +0100

    fix: fix all bugs with api calls

---
+++ mobileApp/lib/globals.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+final ValueNotifier pendingAPI = ValueNotifier(false);
--- mobileApp/lib/main.dart
+++ mobileApp/lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mathiflo/config/config.dart';
 import 'package:mathiflo/constants.dart';
-import 'package:mathiflo/models/groceries_list.dart';
 import 'package:mathiflo/views/CuddlyToys/cuddly_toys_view.dart';
 
 void main() async {
@@ -18,16 +16,13 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => StateNotifierProvider(
-        create: (context) => GroceriesListNotifier(),
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'mathiflo',
-          theme: _theme(),
-          darkTheme: _theme(),
-          home: HookBuilder(
-            builder: (context) => useCuddlyToysView(),
-          ),
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'mathiflo',
+        theme: _theme(),
+        darkTheme: _theme(),
+        home: HookBuilder(
+          builder: (context) => useCuddlyToysView(),
         ),
       );
 }
