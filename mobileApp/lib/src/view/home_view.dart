6fc7d1fa4787a616fa05e33550dd26409c0ab69c
Florent TORNICELLI <florent@geode.com>
2023-04-18 07:40:15 +0200

    chore: new home page design waiting images for cuddly toys and groceries views

--- mobileApp/lib/src/view/home_view.dart
+++ mobileApp/lib/src/view/home_view.dart
@@ -1,44 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:mathiflo/src/widgets/bar.dart';
+import 'package:mathiflo/src/view/CuddlyToys/cuddly_toys_view.dart';
+import 'package:mathiflo/src/view/Groceries/groceries_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: appBar("Bienvenue sur Mathiflo"),
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: GridView(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10,
+        body: ListView(
+          children: [
+            _test(context, 'assets/home_page/example.jpeg', nbItems: 2),
+            _test(context, 'assets/home_page/example.png'),
+            _gridItem(
+              context,
+              "Doudous",
+              Icons.hotel_rounded,
+              Colors.red,
+              const CuddlyToysView(),
             ),
-            children: [
-              _gridItem("Title 1", Icons.home, Colors.red),
-              _gridItem("Title 2", Icons.home, Colors.yellow),
-              _gridItem("Title 3", Icons.home, Colors.green),
-              _gridItem("Title 4", Icons.home, Colors.grey)
-            ],
-          ),
+            _gridItem(
+              context,
+              "Liste de courses",
+              Icons.local_grocery_store,
+              Colors.yellow,
+              const GroceriesView(),
+            ),
+            _gridItem(context, "Feature 3", Icons.home, Colors.green, null),
+            _gridItem(context, "Feature 4", Icons.home, Colors.grey, null),
+            _gridItem(context, "Feature 5", Icons.home, Colors.blue, null),
+            _gridItem(context, "Feature 6", Icons.home, Colors.orange, null),
+            _gridItem(context, "Feature 7", Icons.home, Colors.pink, null),
+            _gridItem(context, "Feature 8", Icons.home, Colors.black, null),
+            _gridItem(context, "Feature 7", Icons.home, Colors.cyan, null),
+            _gridItem(context, "Feature 8", Icons.home, Colors.teal, null)
+          ],
         ),
       );
 }
 
-_gridItem(String title, IconData icon, Color color) => Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: color,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: Colors.white, size: 50),
-          Text(
-            title,
-            style: const TextStyle(color: Colors.white, fontSize: 20),
-          )
-        ],
+_gridItem(
+  BuildContext context,
+  String title,
+  IconData icon,
+  Color color,
+  Widget? direction,
+) =>
+    InkWell(
+      onTap: () async {
+        await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => direction!),
+        );
+      }, // Handle your callback
+      child: SizedBox(
+        height: 100,
+        width: MediaQuery.of(context).size.width,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: color,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, color: Colors.white, size: 50),
+              Text(
+                title,
+                style: const TextStyle(color: Colors.white, fontSize: 20),
+              )
+            ],
+          ),
+        ),
       ),
     );
+
+_test(BuildContext context, String path, {int nbItems = 1}) {
+  print(MediaQuery.of(context).size.width);
+  return Image.asset(
+    path,
+    height: 100.0 * nbItems,
+    width: MediaQuery.of(context).size.width,
+    fit: BoxFit.fill,
+  );
+}
