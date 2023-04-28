49b7c208b56296e7bfcd918932f4a13819459ccb
Florent TORNICELLI <florent@geode.com>
2023-04-28 19:49:24 +0200

    chore: add new configuration for env var in flutter app

--- mobileApp/lib/config.dart
+++ mobileApp/lib/config.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:localstore/localstore.dart';
 
 // Instance of local database
@@ -12,15 +11,15 @@ final localDatabase = Localstore.instance;
 // is waiting for a response from the restAPI
 final ValueNotifier pendingAPI = ValueNotifier(false);
 
+const apiKeyHeader = String.fromEnvironment("MATHIFLO_API_KEY_HEADER");
+const apiKey = String.fromEnvironment("MATHIFLO_API_KEY");
+
 // Url to call rest API
 late String defaultRestApiUrl;
 late String cuddlyToysRestApiUrl;
 late String groceriesRestApiUrl;
 
 Future<void> loadConfigData() async {
-  // Load environment variables
-  await dotenv.load();
-
   // Load config data
   final configString = await rootBundle.loadString('assets/config/config.json');
   final dynamic data = jsonDecode(configString);
